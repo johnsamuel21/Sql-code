d7ee43e37626fb7c72d0e141cd3ef63e553e3d93
johnsamuel21 <61141069+johnsamuel21@users.noreply.github.com>
2020-02-17 14:50:06 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,7 @@
+x=int(input("Enter the numbers..........."))
+if(x>0):
+    print("The Entered Number is postive...")
+elif(x==0):
+    print("The Entered Number is Zero")
+else:
+    print("The Entered Number is Negative")
